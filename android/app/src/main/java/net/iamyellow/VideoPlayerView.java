314599dabcca0dc6c884bad358018a887e288bb5
jordi domenech <jordi@iamyellow.net>
2021-02-04 19:27:30 +0100

    TextureView working :)

--- android/app/src/main/java/net/iamyellow/VideoPlayerView.java
+++ android/app/src/main/java/net/iamyellow/VideoPlayerView.java
@@ -4,21 +4,24 @@ package net.iamyellow;
 // https://github.com/mafanwei/libvlc/blob/cfdff36226870de3b6fd97df0e180b5fce7b2301/vlclibrary/src/main/java/com/mafanwei/vlclibrary/VlcVideoLibrary.java
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 
 import com.vlctests.R;
 
+import org.videolan.libvlc.IVLCVout;
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
-import org.videolan.libvlc.util.VLCVideoLayout;
 
-public class VideoPlayerView extends FrameLayout  {
+public class VideoPlayerView extends FrameLayout implements TextureView.SurfaceTextureListener {
 
-  private VLCVideoLayout mVideoLayout = null;
+  private TextureView mTextureView = null;
   private LibVLC mLibVLC = null;
   private MediaPlayer mPlayer = null;
 
@@ -28,22 +31,43 @@ public class VideoPlayerView extends FrameLayout  {
     LayoutInflater inflater = (LayoutInflater)
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     inflater.inflate(R.layout.iay_video_player_view, this, true);
+    mTextureView = (TextureView) getChildAt(0);
+    mTextureView.setSurfaceTextureListener(this);
 
-    mVideoLayout = (VLCVideoLayout) getChildAt(0);
     mLibVLC = new LibVLC(context);
     mPlayer = new MediaPlayer(mLibVLC);
+
+    mPlayer.setScale(0);
+    mPlayer.setVideoScale(MediaPlayer.ScaleType.SURFACE_BEST_FIT);
+    //mPlayer.setAspectRatio("16:9");
+  }
+
+  @Override
+  public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+    IVLCVout ivlcVout = mPlayer.getVLCVout();
+    ivlcVout.setVideoSurface(surface);
+    ivlcVout.setWindowSize(width, height);
+    ivlcVout.attachViews();
 
     Media media = new Media(mLibVLC, Uri.parse("rtsp://admin:ESVLSW@alfredvpn.mooo.com:5543"));
     media.setHWDecoderEnabled(true, false);
     mPlayer.setMedia(media);
     media.release();
     mPlayer.play();
-    mPlayer.getVLCVout().setWindowSize(300, 200);
-
-    mPlayer.setScale(0);
-    //mPlayer.setVideoScale(MediaPlayer.ScaleType.SURFACE_FIT_SCREEN);
-    //mPlayer.setAspectRatio("16:9");
-    mPlayer.attachViews(mVideoLayout, null, false, false);
   }
 
+  @Override
+  public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+    IVLCVout ivlcVout = mPlayer.getVLCVout();
+    ivlcVout.setWindowSize(width, height);
+  }
+
+  @Override
+  public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+    return false;
+  }
+
+  @Override
+  public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+  }
 }
